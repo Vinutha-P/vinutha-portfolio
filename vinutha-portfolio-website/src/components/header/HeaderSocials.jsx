83866bb6162d60180e1e826f13697fafee8f19a1
Vinutha P <vinuthap@Vinuthas-MacBook-Pro.local>
2023-08-10 14:25:28 +0530

    header-section-completion

---
+++ vinutha-portfolio-website/src/components/header/HeaderSocials.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { BsLinkedin } from "react-icons/bs";
+import { FaGithub } from "react-icons/fa";
+
+const HeaderSocials = () => {
+  return (
+    <div className="header_socials">
+      <a
+        href="https://www.linkedin.com/in/vinutha-softwaredeveloper"
+        target="_blank"
+      >
+        <BsLinkedin />
+      </a>
+      <a href="https://github.com" target="_blank">
+        <FaGithub />
+      </a>
+    </div>
+  );
+};
+
+export default HeaderSocials;
